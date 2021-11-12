eb09348477338b00840ff798b32d884b75ff9e78
bcarancibia <bcarancibia@gmail.com>
2021-11-11 21:44:53 -0500

    Finished out little project

--- Projects/Season Stats Var/Season_Stats_Variance.R
+++ Projects/Season Stats Var/Season_Stats_Variance.R
@@ -11,6 +11,7 @@ library(ggbrace)
 library(magick)
 library(ggtext)
 library(gt)
+library(stringr)
 
 
 
@@ -37,16 +38,16 @@ df_advanced_stats_36 <- NBAPerGameStatisticsPer36Min(season = 2021)
 #NBA Stat R - grab season stats per game to try to show variance
 
 df_2021_gamelogs <- game_logs(seasons = 2021, result_types = "player")
+df_2022_gamelogs <- game_logs(seasons = 2022, result_types = "player")
 
 
-selected_logs <- df_2021_gamelogs %>%
+selected_logs <- df_2022_gamelogs %>%
   select(namePlayer, numberGamePlayerSeason, idPlayer,
          fgm:pctFT, fg2m:fpts)
 
 filter <- selected_logs %>%
   group_by(namePlayer) %>%
-  summarise(minutes = sum(minutes)) %>%
-  filter(minutes > 1000)
+  summarise(minutes = sum(minutes))
 
 player_logs <- selected_logs %>%
   filter(namePlayer %in% filter$namePlayer)
@@ -60,10 +61,22 @@ df_separate <- df %>%
 mvp_stats <- player_logs_separate %>%
   filter(Surname %in% df_separate$Surname)
 
+mvp_stats$namePlayer <- str_c(mvp_stats$Name, ' ', mvp_stats$Surname)
+
+target <- c("Stephen Curry", "Kevin Durant", "Giannis Antetokounmpo",
+            "Luka Doncic", "Joel Embiid", "Nikola Jokic", "Jimmy Butler",
+            "Paul George", "Ja Morant", "Donovan Mitchell")
+
+mvp_stats_top10 <- mvp_stats %>%
+  filter(namePlayer %in% target)
+
 #not working maybe look at gt?
 library(gtExtras)
-plus_minus_sparkline <- player_logs %>%
+plus_minus_sparkline <- mvp_stats_top10 %>%
   dplyr::group_by(namePlayer) %>%
   dplyr::summarise(plus_minus = list(plusminus), .groups = 'drop') %>%
   gt() %>%
+  cols_label(
+    namePlayer = "Player",
+    plus_minus = "Plus Minus") %>%
   gt_sparkline(plus_minus)
